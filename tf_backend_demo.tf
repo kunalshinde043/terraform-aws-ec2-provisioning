30a6b26f2a82e3fbdfd846dba0cbf3865a1e0de0
kunalshinde043 <kunalshinde066@gmail.com>
2025-11-28 22:04:10 +0530

    added state file commands for tfstate

--- tf_backend_demo.tf
+++ tf_backend_demo.tf
@@ -6,7 +6,7 @@ terraform {
   }
 }
 
-resource "aws_instance" "demo_server"{
+resource "aws_instance" "demo_server_fs"{
     ami = var.my_ami
     instance_type = var.my_instance_type
     key_name = var.key_name
@@ -15,3 +15,16 @@ resource "aws_instance" "demo_server"{
       Name = "MyAppServer"
     }
 }
+
+
+# Commands for Reading tfstate file
+
+# 1)terraform state pull --> pulls complete tf state file as it is from s3.
+
+# 2)terraform state list --> lists resources inside our tfstate file.
+
+# 3)terraform state show <resource address>aws_instance.demo_server --> shows complete details for specific resource only.
+
+# 4)terraform state mv aws_instance.server_1 aws_instance.main_server --> to update resource level config changes such as logical name of resource
+
+# 5)terraform state rm aws_instance.server_1 --> to remove resouce from tfstate file hence no more tracking would be done(also remove from config file)
